9368212f48cdaa84358ef2b13cf6910e4d6e5dd5
Tom Nixon <tom@tomn.co.uk>
2019-08-14 12:17:29 +0100

    speedup time parse, but make format more lenient

--- src/elements/time.cpp
+++ src/elements/time.cpp
@@ -7,23 +7,57 @@
 namespace adm {
 
   std::chrono::nanoseconds parseTimecode(const std::string& timecode) {
-    const static std::regex commonFormat("(\\d{2}):(\\d{2}):(\\d{2}).(\\d{1,9})");
-    std::smatch timecodeMatch;
-    if (std::regex_match(timecode, timecodeMatch, commonFormat)) {
-      // add trailing zeros to string
-      auto nanoseconds =
-          boost::str(boost::format("%1%") %
-                     boost::io::group(std::setw(9), std::left,
-                                      std::setfill('0'), timecodeMatch[4]));
-      return std::chrono::hours(stoi(timecodeMatch[1])) +
-             std::chrono::minutes(stoi(timecodeMatch[2])) +
-             std::chrono::seconds(stoi(timecodeMatch[3])) +
-             std::chrono::nanoseconds(stoi(nanoseconds));
-    } else {
-      std::stringstream errorString;
-      errorString << "invalid timecode: " << timecode;
-      throw std::runtime_error(errorString.str());
+    // find pos of decimal point, and check there's only one
+    size_t dp_pos = timecode.size();
+    for (size_t i = 0; i < timecode.size(); i++) {
+      if (timecode[i] == '.') {
+        if (dp_pos != timecode.size()) {
+          throw std::runtime_error("invalid timecode: " + timecode);
+        }
+
+        dp_pos = i;
+      }
     }
+
+    uint64_t total_ns = 0;
+
+    // parse the part after the decimal point
+    {
+      uint64_t place_value = 100000000L;
+      for (size_t i = dp_pos + 1; i < timecode.size(); i++) {
+        char c = timecode[i];
+
+        if ('0' <= c && c <= '9') {
+          total_ns += place_value * (c - '0');
+          place_value /= 10;
+        } else {
+          throw std::runtime_error("invalid timecode: " + timecode);
+        }
+      }
+    }
+
+    // parse the ':' seperated parts before the decimal point, working backwards
+    {
+      uint64_t part_value = 1000000000L;
+      uint64_t place_value = part_value;
+      uint64_t num_parts = 0;
+      for (int i = dp_pos - 1; i >= 0; i--) {
+        char c = timecode[i];
+
+        if ('0' <= c && c <= '9') {
+          total_ns += place_value * (c - '0');
+          place_value *= 10;
+        } else if (c == ':' && num_parts < 2) {
+          part_value *= 60;
+          place_value = part_value;
+          num_parts++;
+        } else {
+          throw std::runtime_error("invalid timecode: " + timecode);
+        }
+      }
+    }
+
+    return std::chrono::nanoseconds(total_ns);
   }
 
   std::string formatTimecode(const std::chrono::nanoseconds& time) {
